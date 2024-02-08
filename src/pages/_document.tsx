ede6727d50657527d19c7fa5a6693a030e8825b8
pjiang604 <patriciaejiang@gmail.com>
2024-02-07 21:21:47 -0800

    Add back in top header component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 
 export default function Document() {
   return (
@@ -12,6 +13,7 @@ export default function Document() {
             </header>
         </Head>
       <body>
+        <Header/>
         <Main />
         <NextScript />
 

94e51c59f71fb1b0868f34b8f8570d4cf44da613
pjiang604 <patriciaejiang@gmail.com>
2024-02-07 21:07:21 -0800

    comment out Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,13 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
+      {/* <Head>
         <header>
           <title>Nimbus Weather App</title>
           <meta name='author' content='Patricia Jiang' />
           <meta name='description' content='Interactive weather web app' />
         </header>
-      </Head>
+      </Head> */}
       <Component {...pageProps} />
     </>
 

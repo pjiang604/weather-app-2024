48c88804cb6d9508ff6d8f38a8b9fb4fb3c05cfa
pjiang604 <patriciaejiang@gmail.com>
2024-02-07 21:19:35 -0800

    Edit Header tag error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,7 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <Head>
-        <header>
-          <title>Nimbus Weather App</title>
-          <meta name='author' content='Patricia Jiang' />
-          <meta name='description' content='Interactive weather web app' />
-        </header>
-      </Head> */}
+
       <Component {...pageProps} />
     </>
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+            <header>
+                <title>Nimbus Weather App</title>
+                <meta name='author' content='Patricia Jiang' />
+                <meta name='description' content='Interactive weather web app' />
+            </header>
+        </Head>
       <body>
-        <Header />
         <Main />
         <NextScript />
 

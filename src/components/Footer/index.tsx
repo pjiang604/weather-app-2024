2881016d1d1327456b388f3931d6b28b2930dd25
pjiang604 <patriciaejiang@gmail.com>
2024-02-08 11:45:02 -0800

    replaced div with footer tag in footer element

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,8 +2,8 @@
 
 export default function Footer() {
     return (
-        <div id="footer" className={`bg-lightTeal sticky bottom-0 w-full`}>
+        <footer id="footer" className={`bg-lightTeal sticky bottom-0 w-full`}>
             <p className={`text-offWhite`}>Created by Patricia Jiang</p>
-        </div>
+        </footer>
     )
 }
